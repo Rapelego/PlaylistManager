458c4e759253e225f1dd83291ca177eed21127fd
Rapelego <st10496347@rcconnect.edu.za>
2025-06-19 11:04:20 +0200

    Error Handling and adding second screen

--- app/src/main/java/com/example/playlistmanager/MainActivity.kt
+++ app/src/main/java/com/example/playlistmanager/MainActivity.kt
@@ -2,6 +2,8 @@
 package com.example.playlistmanager
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +18,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.navigation.NavController
-
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.*
 import androidx.navigation.compose.rememberNavController
 import com.example.playlistmanager.ui.theme.PlaylistManagerTheme
@@ -47,5 +51,51 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun MainScreen(navController: NavController){
-    
+    //Input states for form fields
+    var song by remember { mutableStateOf("") }
+    var artist by remember { mutableStateOf("") }
+    var ratings by remember { mutableStateOf("") }
+    var comments by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column (modifier = Modifier.padding(16.dp)) {
+        Text("Song Name",style = MaterialTheme.typography.titleLarge)
+        Spacer(modifier = Modifier.height(8.dp))
+
+        //Input fields
+        OutlinedTextField(value = song, onValueChange = {song=it},label = { Text("Song") })
+        OutlinedTextField(value = artist, onValueChange = {artist=it},label = { Text("Artist") })
+        OutlinedTextField(value = ratings, onValueChange = {ratings = it}, label = { Text("Ratings") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+        )
+        OutlinedTextField(value = comments, onValueChange = {comments = it}, label = {Text("Comments")} )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        //Button to add Name of the song
+        Button(onClick = ) {
+            //Vallidates input before saving
+            if (song.isNotBlank() && artist.isNotBlank() && ratings.toIntOrNull() !=null && comments.isNotBlank()) {
+                //Save to parallel arrays
+                songList.add(song)
+                artistList.add(artist)
+                ratingsList.add(ratings.toInt())
+                commentsList.add(comments)
+
+                //Log the data to Logcat
+                Log.i("PlaylistManager","Song added: $song,$artist,$ratings,$comments")
+
+                //Notify user if successful
+                Toast.makeText(context,"Song Added", Toast.LENGTH_SHORT).show()
+
+                //Reset fields
+                song= "" ; artist= "" ; ratings = "" ; comments = ""
+            } else{
+                //Display error if validation fails
+                Toast.makeText(context,"Please all song details correctly.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
