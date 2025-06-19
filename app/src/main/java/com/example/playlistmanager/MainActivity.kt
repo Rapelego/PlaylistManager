4d804d866e0e8b3ef2867c130463606155d57ad0
Rapelego <st10496347@rcconnect.edu.za>
2025-06-19 09:31:23 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/playlistmanager/MainActivity.kt
+++ app/src/main/java/com/example/playlistmanager/MainActivity.kt
@@ -1,9 +1,9 @@
+//ST10496347_MADITSI RAPELEGO
 package com.example.playlistmanager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,11 +13,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.playlistmanager.ui.theme.PlaylistManagerTheme
 
+//Parallel arrays
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+
+
+
+
+
+
+
 
         }
     }
